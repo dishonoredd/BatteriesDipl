cf8777d1163e68598c8f5216850eade401474118
dishonoredd <suiratsasha06@gmail.com>
2026-05-27 17:23:10 +0300

    с

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
           href={homePath}
           className="text-base sm:text-sm md:text-lg text-neutral-700 hover:text-neutral-800"
         >
-          Аккумуляторы
+          Название сайта
         </Link>
       </div>
     </header>
